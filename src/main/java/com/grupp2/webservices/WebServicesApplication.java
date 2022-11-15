0034ed1376de6c5889840c199f1543179c6a2722
MaxAndreasLundin <lundin-89@hotmail.com>
2022-11-15 13:58:37 +0100

    Initial commit

---
+++ src/main/java/com/grupp2/webservices/WebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.grupp2.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServicesApplication.class, args);
+    }
+
+}
