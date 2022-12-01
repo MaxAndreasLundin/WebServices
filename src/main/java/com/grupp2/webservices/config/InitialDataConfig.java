04f8390e1ce83475f88fe352e71c1ad2e5d65261
Nina Odelius <91470981+ninaodelius@users.noreply.github.com>
2022-12-01 11:27:12 +0100

    fix

--- src/main/java/com/grupp2/webservices/config/InitialDataConfig.java
+++ src/main/java/com/grupp2/webservices/config/InitialDataConfig.java
@@ -13,7 +13,7 @@ public class InitialDataConfig {
     @Bean
     CommandLineRunner commandLineRunner(UserRepository userRepository){
         return args -> {
-            User user1 = new User("Nina", "Test", "nina@mail.com", "password");
+            User user1 = new User("Benni", "Beany", "deezbeenz@mail.com", "password");
             User user2 = new User("Arne", "Weise", "arne@mail.com", "password");
             userRepository.saveAll(List.of(user1,user2));
         };
